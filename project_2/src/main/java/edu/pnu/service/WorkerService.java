126b42b5b6194dd46e2d224785aa4e6dd942d744
sweethedge <sweethedge@naver.com>
2023-02-17 15:07:53 +0900

    처음 STS로 해보는 커밋

---
+++ project_2/src/main/java/edu/pnu/service/WorkerService.java
@@ -0,0 +1,21 @@
+package edu.pnu.service;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import edu.pnu.dao.WorkerDAO;
+import edu.pnu.domain.WorkerVO;
+
+@Service
+public class WorkerService {
+
+	@Autowired
+	WorkerDAO wd;
+
+	public List<WorkerVO> getWorkerList() {
+		return wd.getWorkerList();
+	}
+}
