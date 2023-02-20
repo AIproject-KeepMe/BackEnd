6cfc078f484ca31020ddb02082e148211696f842
sweethedge <sweethedge@naver.com>
2023-02-20 11:05:58 +0900

    -

---
+++ src/components/Table.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function MyComponent() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('http://localhost:8080/data/workerlist');
+      setData(result.data);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>My Data</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>User</th>
+            <th>Stat</th>
+            <th>Label</th>
+            <th>Recorded Time</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map(item => (
+            <tr key={item.id}>
+              <td>{item.user}</td>
+              <td>{item.stat}</td>
+              <td>{item.stat_label}</td>
+              <td>{item.recorded_time}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default MyComponent;
